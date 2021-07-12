9e80e8794fc73ad3c1b7e9b94f13f7f28f72bc6f
JorgeLins <jorgelins1155@gmail.com>
2021-07-12 20:52:57 -0300

    First commit alurakut

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,33 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { AlurakutStyles } from '../src/lib/AlurakutCommons'
 
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    background-color: #D9E6F6;
   }
+
+  #__next {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+    display: block;
+  }
+
+  ${AlurakutStyles}
 `
 
 const theme = {
   colors: {
-    primary: '#0070f3',
+    primary: '#FFF',
   },
 }
 
